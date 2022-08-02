36797e65ee27380c9b42fda5fd47f777bfc941d2
bscott4 <bscott4.dev@gmail.com>
2022-08-02 17:22:32 -0400

    ⚠️ instantiating apollo client requires headers * this was a bit of a gotcha! the error message was not intuitive 😨

--- antler-onboarding/pages/_app.tsx
+++ antler-onboarding/pages/_app.tsx
@@ -1,9 +1,18 @@
-import "../styles/globals.css";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   const client = new ApolloClient({
-    uri: "http://localhost:4000/v1/graphql",
+    link: new HttpLink({
+      uri: "http://localhost:4000/v1/graphql",
+      headers: {
+        "x-hasura-admin-secret": "secret",
+      },
+    }),
     cache: new InMemoryCache(),
   });
 
@@ -12,6 +21,6 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
     </ApolloProvider>
   );
-}
+};
 
 export default MyApp;
